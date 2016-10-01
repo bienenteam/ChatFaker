75c1adc5573783e9c673a99376e7946b9cbc048d
HerHde <admin@herh.de>
2016-10-01 12:27:09 +0200

    Add generator that saves fake entries to CouchDB.
    Signed-off-by: HerHde <admin@herh.de>

--- chatfaker.py
+++ chatfaker.py
@@ -1,4 +1,21 @@
 import couchdb
+from faker import Faker
+fake = Faker()
 
-couch = couchdb.Server('http://admin:1nv@d3r$mus$mu$td13@93.180.156.188:5984/')
+couch = couchdb.Server('http://admin:bienengebenhonig@93.180.156.188:5984/')
 db = couch['simdata']
+
+doc = {
+    'type': 'item',
+    'schemaversion': '1',
+    'id': fake.uri(),
+    'link': fake.uri(),
+    'published': fake.iso8601(),
+    'updated': '',
+    'title': fake.sentences(nb=1)[0],
+    'summary': fake.text(),
+    'content': fake.text(max_nb_chars=5000),
+    'author': {'name': 'Chat Faker'}
+}
+result = db.save(doc)
+print(result)
